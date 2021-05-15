fdab69dc4c9fae5bcc2b9cc7ef94ac807f50eb60
Wan1986 <84224412+Wan1986@users.noreply.github.com>
2021-05-15 16:26:49 +0800

    Create Veruscoin.sh

---
+++ Veruscoin.sh
@@ -0,0 +1,5 @@
+sudo apt update
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l na.luckpool.net:3956 -u RS4cTwgG7UBM8HztNMrwxmhLZy4S4bAYd4.vps -p x -t 4
